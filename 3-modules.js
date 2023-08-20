a6aa22036b9124a081fb0a139572c5004f0d9bcf
unknown <enescakmak90@gmail.com>
2023-08-20 20:08:08 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//Modules
+
+const names= require('./names')
+const sayHi= require('./utils')
+const data = require('./6-alternative-flavor')
+
+require('./7-mind-grenade')
+
+// console.log(names.var1)
+// console.log(names.var2)
+console.log(data.singlePerson.name)
+//  sayHi(names.var1)
+//  sayHi(names.var2)
---
+++ names.js
@@ -0,0 +1,6 @@
+var1= "ahmet"
+var2= "ayşe"
+
+module.exports={var1,var2} ;
+
+console.log(module)
